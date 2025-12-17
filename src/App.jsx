19ea258e01e959f21449216b74b8728b332ecfbc
Akhil R <akhil.r@zeronetraining.local>
2025-12-17 17:40:59 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  closestCorners,
+} from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+
+import { Input } from "./components/Input/Input";
+import { Column } from "./components/Column/Column";
+
+import "./App.css";
+
+export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Study" },
+    { id: 2, title: "Workout" },
+    { id: 3, title: "Learn a new language" },
+  ]);
+
+  const addTask = (title) => {
+    setTasks((tasks) => [...tasks, { id: tasks.length + 1, title }]);
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const getTaskPos = (id) => tasks.findIndex((task) => task.id === id);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id === over.id) return;
+
+    setTasks((tasks) => {
+      const originalPos = getTaskPos(active.id);
+      const newPos = getTaskPos(over.id);
+
+      return arrayMove(tasks, originalPos, newPos);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>My Tasks</h1>
+      <Input onSubmit={addTask} />
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCorners}
+        onDragEnd={handleDragEnd}
+      >
+        <Column id="toDo" tasks={tasks} />
+      </DndContext>
+    </div>
+  );
+}
